4f30ca51774ab12d5b003a2f7511f7a66f9a1995
Kaj <kaaiman99@hotmail.com>
2017-10-10 20:36:27 +0200

    Initial commit

---
+++ Assets/GetSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSprite : MonoBehaviour {
+	public Sprite sprite;
+
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	void Update () {
+		
+	}
+}
